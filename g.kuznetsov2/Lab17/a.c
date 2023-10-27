1e940c97ac3a0204ae6152f408d85979b7549b60
Zea-Zee <glebru.irk38@yandex.ru>
2023-10-28 00:36:32 +0700

    17

--- g.kuznetsov2/Lab17/a.c
+++ g.kuznetsov2/Lab17/a.c
@@ -1,8 +1,10 @@
 #include <sys/ioctl.h>
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+//#include <conio.h>
 #include <errno.h>
 #define BUF_SIZE 100
 
@@ -31,13 +33,36 @@ void TS_init(TS *ts){
 }
 
 void TS_append(TS *ts, char ch){
+//    char tmp = '|';
+//    if(ch == '\n') write(STDOUT_FILENO, &tmp, sizeof(char));
+    if(!isprint(ch) && ch != '\n') return;
     if(ts->length + 1 == ts->size){
         ts->str = (char*) realloc(ts->str, sizeof(char) * ts->size + ts->cols);
         ts->size += ts->cols;
-        TS_append(ts, '\n');
+        TS_append(ts, ch);
     }
     write(STDOUT_FILENO, &ch, sizeof(char));
+//    printf(":%c|%d:", ch, ts->length);
     ts->str[ts->length++] = ch;
+}
+
+void TS_erase(TS *ts){
+    if(ts->length == 0) return;
+    ts->length--;
+    write(STDOUT_FILENO, "\b \b", sizeof(char) * 3);
+}
+
+void TS_kill(TS *ts){
+    if(ts->length == 0) return;
+    while(ts->length > 0 && ts->str[ts->length] != '\n'){
+        ts->length--;
+        write(STDOUT_FILENO, "\b \b", sizeof(char) * 3);
+    }
+}
+
+void type_kill(){
+    char c = 21; // ASCII value of CTRL-U (VKILL)
+    ioctl(STDIN_FILENO, TIOCSTI, &c);
 }
 
 int main() {
@@ -54,7 +79,7 @@ int main() {
 
     char ch;
     while (1){
-        if(read(fileno(stdin), &ch, sizeof(char) == -1)){
+        if(read(fileno(stdin), &ch, sizeof(char)) == -1){
             if(errno == EINTR){
 //                perror("Something went wrong in reading, trying again\n");
                 continue;
@@ -65,8 +90,25 @@ int main() {
             perror("Can't open the file :(\n");
             exit(1);
         }
-        if(ch == '\000') continue;
-        TS_append(&ts, ch);
+//        printf("%d ", ts.length);
+//        int intChar = getchar();
+//        printf("%d", intChar);
+//        if(intChar == EOF) break;
+//        if(ch == '\000') continue;
+//        printf("%c", ch);
+//        if(ts.length == 16){
+//            type_kill();
+//            ts.length++;
+//        }
+        else if(terminal_settings.c_cc[VERASE] == ch) TS_erase(&ts);
+        else if(terminal_settings.c_cc[VKILL] == ch) TS_kill(&ts);
+        else if('\n' == ch)
+            TS_append(&ts, '\n');
+        else if(isprint(ch)){
+            TS_append(&ts, ch);
+        } else{
+            write(STDOUT_FILENO, "\a", sizeof(char));
+        }
     }
 //
 //    char *buf = (char*) calloc(BUF_SIZE, sizeof(char));
